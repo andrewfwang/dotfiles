b80a1cea07b1283eb875f58beec6430b5a038b20
Andrew Wang <andrewfwang@gmail.com>
2015-02-22 09:53:11 -0800

    make undo ctrl+z for consistency with other programs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,14 +20,25 @@
 
 (setq default-fill-mode 80)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.  If you edit it by hand,
+ ;; you could mess it up, so be careful.  Your init file should
+ ;; contain only one such instance.  If there is more than one, they
+ ;; won't work right.
  '(tool-bar-mode nil))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.  If you edit it by hand, you
+ ;; could mess it up, so be careful.  Your init file should contain
+ ;; only one such instance.  If there is more than one, they won't
+ ;; work right.
  )
+
+;; custom shortcuts
+
+;; Unbind Pesky Sleep Button
+(global-unset-key [(control z)])
+(global-unset-key [(control x)(control z)])
+
+;; Windows Style Undo
+(global-set-key [(control z)] 'undo)
+
+
